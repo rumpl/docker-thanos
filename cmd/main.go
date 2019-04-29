eac6bf67d10a84b6274e30011103f1fd63f18672
Djordje Lukic <lukic.djordje@gmail.com>
2019-04-29 21:46:10 +0200

    chore: run goimports on code

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
+
 	"github.com/docker/cli/cli-plugins/manager"
 	"github.com/docker/cli/cli-plugins/plugin"
 	"github.com/docker/cli/cli/command"
 	cliflags "github.com/docker/cli/cli/flags"
 	"github.com/docker/docker/api/types"
 	"github.com/spf13/cobra"
-	"math/rand"
 )
 
 func snap(containers []types.Container) []types.Container {
@@ -17,7 +18,7 @@ func snap(containers []types.Container) []types.Container {
 	rand.Shuffle(len(containers), func(i, j int) {
 		sample[i], sample[j] = sample[j], sample[i]
 	})
-	sample = sample[:len(containers) / 2]
+	sample = sample[:len(containers)/2]
 	return sample
 }
 
@@ -47,7 +48,8 @@ func run() {
 
 	fmt.Println("Fun isn’t something one considers when balancing the universe. But this… does put a smile on my face.")
 }
-func main () {
+
+func main() {
 	plugin.Run(func(dockerCli command.Cli) *cobra.Command {
 		cmd := &cobra.Command{
 			Short: "",
